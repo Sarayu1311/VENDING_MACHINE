a823a0997c6692da0f00ba2c789717e5ddc27fd5
Sarayu1311 <sarayusri.pydimarri@gmail.com>
2025-04-05 16:31:05 +0530

    Update test.v

--- test.v
+++ test.v
@@ -18,6 +18,50 @@
 // Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
+module vendor_tb;
+    reg clk, reset, coin_1, coin_2;
+    wire dispense, change;
+    
+    vendor uut (
+        .clk(clk),
+        .reset(reset),
+        .coin_1(coin_1),
+        .coin_2(coin_2),
+        .dispense(dispense),
+        .change(change)
+    );
+    
+    initial begin
+        clk = 0;
+        forever #5 clk = ~clk;
+    end
+    
+    initial begin
+        reset = 1;
+        coin_1 = 0;
+        coin_2 = 0;
+        
+        #10 reset = 0;
+        
+        #10 coin_1 = 1; #10 coin_1 = 0;
+        #20 coin_1 = 1; #10 coin_1 = 0;
+        #20 coin_1 = 1; #10 coin_1 = 0;
+        
+        #20;
+        
+        reset = 1; #10 reset = 0;
+        
+        #10 coin_2 = 1; #10 coin_2 = 0;
+        #20 coin_1 = 1; #10 coin_1 = 0;
+        
+        #20;
+        
+        reset = 1; #10 reset = 0;
+        #10 coin_2 = 1; #10 coin_2 = 0;
+        #20 coin_2 = 1; #10 coin_2 = 0;
+        
+        #100 $stop;
+    end
+endmodule
 
 
